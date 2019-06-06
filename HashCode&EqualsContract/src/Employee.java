5571f9464b92ba2da168bf16d8120740cab3a9ee
sdiwan1992 <38852617+sdiwan1992@users.noreply.github.com>
2019-06-06 18:14:17 +0530

    Initial Commit

---
+++ HashCode&EqualsContract/src/Employee.java
@@ -0,0 +1,69 @@
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String companyName;
+	public Employee(int empId, String empName, String companyName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.companyName = companyName;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", companyName=" + companyName + "]";
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((companyName == null) ? 0 : companyName.hashCode());
+		result = prime * result + empId;
+		result = prime * result + ((empName == null) ? 0 : empName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (companyName == null) {
+			if (other.companyName != null)
+				return false;
+		} else if (!companyName.equals(other.companyName))
+			return false;
+		if (empId != other.empId)
+			return false;
+		if (empName == null) {
+			if (other.empName != null)
+				return false;
+		} else if (!empName.equals(other.empName))
+			return false;
+		return true;
+	}
+	
+	
+}
